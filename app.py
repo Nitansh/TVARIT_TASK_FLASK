a4008f75babdebe5841b300560b9dea1efc928b5
Nitansh Bareja <nibarej1@in.ibm.com>
2021-07-26 19:11:50 +0530

    Adding the missing file and moving the application to flask code
    Signed-off-by: Nitansh Bareja <nibarej1@in.ibm.com>

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 # Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13...19
 # inclusive -- then that value counts as 0, except 15 and 16 do not count as a teen. The input is passed as
 # command line arguments and output is to be printed on screen
-import sys
+import json
+from flask import Flask, request
 
 from constant import ACTUAL_TEEN_LIST, ERROR_MSG_INVALID_INPUT, ERROR_MSG_INVALID_COUNT_ARGS, ARGS_ALLOWED
 from validation import validate_arguments_length, get_converted_args
 from logic import calculate_sum
 
-if __name__ == "__main__":
-  args = sys.argv
-  actual_args = args[ 1: ] #ARGS also include the filename of python file
+app = Flask(__name__)
+
+@app.route( '/sum', methods=['POST'] )
+def main():
+  actual_args = request.get_json()
   if validate_arguments_length( actual_args, ARGS_ALLOWED ):
     if get_converted_args( actual_args ):
-      print( calculate_sum( get_converted_args( actual_args ), ACTUAL_TEEN_LIST ) )
+      return app.response_class(
+        response=json.dumps( { "result" : calculate_sum( get_converted_args( actual_args ), ACTUAL_TEEN_LIST ) } ),
+        status=200,
+        mimetype='application/json',
+      )
     else :
-      print ( ERROR_MSG_INVALID_INPUT )
+      return app.response_class(
+        response=json.dumps( { 'error' : ERROR_MSG_INVALID_INPUT } ),
+        status=400,
+        mimetype='application/json',
+      )
   else:
-    print( ERROR_MSG_INVALID_COUNT_ARGS )
+    return app.response_class(
+      response=json.dumps( { 'error' : ERROR_MSG_INVALID_COUNT_ARGS } ),
+      status=400,
+      mimetype='application/json',
+    )
+
+if __name__ == '__main__':
+   app.run( debug = False )
